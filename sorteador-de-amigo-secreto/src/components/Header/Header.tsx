016e7a2f3cad594724ef61278df93b3a82a4aa46
Filipe <fgallo_dev@hotmail.com>
2023-02-06 10:36:55 -0300

    Header adicionado

---
+++ sorteador-de-amigo-secreto/src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div
+        className="imagem-logo"
+        role="img"
+        aria-label="Logo do Sorteador"
+      ></div>
+      <img
+        className="participante"
+        src="/imagens/participante.png"
+        alt="Participante com um presente na mão"
+      />
+    </header>
+  );
+};
+
+export default Header;
