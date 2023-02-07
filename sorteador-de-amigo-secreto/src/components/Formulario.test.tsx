8426077c0289821cb6fe11d10778c17eb471009a
Filipe <filipegallodev@hotmail.com>
2023-02-07 10:40:22 -0300

    Testando o comportamento

--- sorteador-de-amigo-secreto/src/components/Formulario.test.tsx
+++ sorteador-de-amigo-secreto/src/components/Formulario.test.tsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Formulario from "./Formulario";
 
-test("quando o input está vazio, novos participantes não podem ser adicionados", () => {
+test("Quando o input está vazio, novos participantes não podem ser adicionados", () => {
   render(<Formulario />);
-
   const input = screen.getByPlaceholderText(
     "Insira os nomes dos participantes"
   );
   const button = screen.getByRole("button");
-
   expect(input).toBeInTheDocument();
   expect(button).toBeDisabled();
+});
+
+test("Adicionar um participante caso exista um nome preenchido", () => {
+  render(<Formulario />);
+  const input = screen.getByPlaceholderText(
+    "Insira os nomes dos participantes"
+  );
+  const button = screen.getByRole("button");
+  fireEvent.change(input, {
+    target: {
+      value: "Filipe",
+    },
+  });
+  fireEvent.click(button);
+  expect(input).toHaveFocus();
+  expect(input).toHaveValue("");
 });
