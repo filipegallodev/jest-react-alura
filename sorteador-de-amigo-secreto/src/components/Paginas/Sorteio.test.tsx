20de703b0d4438e2c9b0b0d989165bce7bbb894b
Filipe <fgallo_dev@hotmail.com>
2023-02-15 08:32:05 -0300

    Exibindo o amigo secreto

--- sorteador-de-amigo-secreto/src/components/Paginas/Sorteio.test.tsx
+++ sorteador-de-amigo-secreto/src/components/Paginas/Sorteio.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { useListaDeParticipantes } from "../../state/hooks/useListaDeParticipantes";
+import { useResultadoSorteio } from "../../state/hooks/useResultadoSorteio";
 import Sorteio from "./Sorteio";
 
 jest.mock("../../state/hooks/useListaDeParticipantes", () => {
@@ -10,11 +11,25 @@ jest.mock("../../state/hooks/useListaDeParticipantes", () => {
   };
 });
 
+jest.mock("../../state/hooks/useResultadoSorteio", () => {
+  return {
+    useResultadoSorteio: jest.fn(),
+  };
+});
+
 describe("Na pagina de sorteio", () => {
   const participantes = ["Joel", "Ellie", "Tes"];
+  const resultado = new Map([
+    ["Tes", "Joel"],
+    ["Ellie", "Tes"],
+    ["Joel", "Ellie"],
+  ]);
+
   beforeEach(() => {
     (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+    (useResultadoSorteio as jest.Mock).mockReturnValue(resultado);
   });
+
   test("Todos os participantes podem exibir o seu amigo secreto", () => {
     render(
       <RecoilRoot>
@@ -23,5 +38,23 @@ describe("Na pagina de sorteio", () => {
     );
     const options = screen.queryAllByRole("option");
     expect(options).toHaveLength(participantes.length);
+  });
+
+  test("O amigo secreto é exibido quando solicitado", () => {
+    render(
+      <RecoilRoot>
+        <Sorteio />
+      </RecoilRoot>
+    );
+    const select = screen.getByPlaceholderText("Selecione o seu nome");
+    fireEvent.change(select, {
+      target: {
+        value: participantes[0],
+      },
+    });
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    const amigoSecreto = screen.getByRole("alert");
+    expect(amigoSecreto).toBeInTheDocument();
   });
 });
--- sorteador-de-amigo-secreto/src/components/Paginas/Sorteio.tsx
+++ sorteador-de-amigo-secreto/src/components/Paginas/Sorteio.tsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useListaDeParticipantes } from "../../state/hooks/useListaDeParticipantes";
+import { useResultadoSorteio } from "../../state/hooks/useResultadoSorteio";
 
 const Sorteio = () => {
+  const [participanteDaVez, setParticipanteDaVez] = useState("");
+  const [amigoSecreto, setAmigoSecreto] = useState("");
   const participantes = useListaDeParticipantes();
+  const resultado = useResultadoSorteio();
+
+  function sortear(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (resultado && resultado.has(participanteDaVez))
+      setAmigoSecreto(resultado.get(participanteDaVez)!);
+  }
 
   return (
     <section>
-      <form>
-        <select name="participante-da-vez" id="participante-da-vez">
+      <form onSubmit={sortear}>
+        <select
+          required
+          name="participante-da-vez"
+          id="participante-da-vez"
+          placeholder="Selecione o seu nome"
+          value={participanteDaVez}
+          onChange={({ target }) => setParticipanteDaVez(target.value)}
+        >
           {participantes.map((participante) => (
             <option key={participante}>{participante}</option>
           ))}
         </select>
+        <button>Sortear</button>
       </form>
+      {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
     </section>
   );
 };
